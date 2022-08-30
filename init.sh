c6e8130da2e9a2e75fb630a3c5074f1db4ee28da
Jeremy A. Puhlman <jpuhlman@mvista.com>
2022-08-30 14:28:47 -0700

    Remove mock

--- init.sh
+++ init.sh
@@ -7,8 +7,6 @@ fi
 groupadd -g $USERGID engr 
 useradd -s /bin/bash -d $HOME -M -u $USERID -g $USERGID $USERNAME
 echo "$USERNAME  ALL=(ALL)       NOPASSWD: ALL" | tee -a /etc/sudoers > /dev/null
-sudo usermod -a -G mock $USERNAME
-echo "export MOCK_OPTS='--old-chroot'" >> /etc/bashrc
 echo
 echo "Welcome to $RELEASE_DISTRO $RELEASE_VERSION builder"
 echo
